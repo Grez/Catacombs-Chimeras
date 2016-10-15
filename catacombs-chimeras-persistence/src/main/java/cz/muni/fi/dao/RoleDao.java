305b95611339c668363113d168be6982a9689274
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2016-10-15 22:45:17 +0200

    fixup! hibernate spring config and added role dao

--- catacombs-chimeras-persistence/src/main/java/cz/muni/fi/dao/RoleDao.java
+++ catacombs-chimeras-persistence/src/main/java/cz/muni/fi/dao/RoleDao.java
@@ -8,10 +8,41 @@ import java.util.List;
  * @author Ondřej Benkovský
  */
 public interface RoleDao {
+    /**
+     * creates new role
+     * @param role
+     */
     void create(final Role role);
+
+    /**
+     * delete role
+     * @param role
+     */
     void delete(final Role role);
+
+    /**
+     * find role by ID
+     * @param id
+     * @return role if found, else null
+     */
     Role findById(final Long id);
+
+    /**
+     * find role by name
+     * @param name
+     * @return role if found, else null
+     */
     Role findByName(final String name);
+
+    /**
+     * find all roles
+     * @return
+     */
     List<Role> findAll();
+
+    /**
+     * update role
+     * @param role
+     */
     void update(final Role role);
 }

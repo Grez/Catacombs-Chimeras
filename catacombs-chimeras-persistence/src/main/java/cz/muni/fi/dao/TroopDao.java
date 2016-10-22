6661574b65e27b2ea111fc1f24923bdac01ea75e
david <ddoa14@seznam.cz>
2016-10-22 18:13:24 +0200

    Troop entity implemented

---
+++ catacombs-chimeras-persistence/src/main/java/cz/muni/fi/dao/TroopDao.java
@@ -0,0 +1,49 @@
+package cz.muni.fi.dao;
+
+import cz.muni.fi.entity.Troop;
+
+import java.util.List;
+
+/**
+ *
+ * @author David Osička
+ */
+public interface TroopDao {
+    /**
+     * Creates new entity of Troop in data layer
+     * @param instance is saved to data layer
+     */
+    void create(final Troop instance);
+    
+    /**
+     * Updates already existing entity of Troop
+     * @param instance is updated in data layer
+     */
+    void update(final Troop instance);
+    
+    /**
+     * Deletes already existing entity of Troop
+     * @param instance is deleted from data layer
+     */
+    void delete(final Troop instance);
+    
+    /**
+     * Finds entity of Troop by its ID
+     * @param id ID to find the entity
+     * @return entity(instance) of Troop found by this method
+     */
+    Troop findById(final Long id);
+    
+    /**
+     * Finds entity of Troop by its name
+     * @param name to find the entity
+     * @return entity(instance) of Troop found by this method
+     */
+    Troop findByName(final String name);
+    
+    /**
+     * Finds all entities of Troop
+     * @return list of all troops
+     */
+    List<Troop> findAll();
+}
---
+++ catacombs-chimeras-persistence/src/main/java/cz/muni/fi/dao/TroopDaoImpl.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.dao;
+
+import cz.muni.fi.entity.Troop;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.apache.commons.lang3.Validate;
+
+
+/**
+ *
+ * @author David Osička
+ */
+
+@Repository
+public class TroopDaoImpl implements TroopDao {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    public void create(final Troop instance) {
+        Validate.notNull(instance, "instance must not be null");
+        entityManager.persist(instance);
+    }
+    
+    public void update(final Troop instance) {
+        Validate.notNull(instance, "instance must not be null");
+        entityManager.remove(instance);
+    }
+    
+    public void delete(final Troop instance) {
+        Validate.notNull(instance, "instance must not be null");
+        entityManager.remove(instance);
+    }
+    
+    public Troop findById(final Long id) {
+        Validate.notNull(id, "id must not be null");
+        return entityManager.find(Troop.class, id);
+    }
+    
+    public Troop findByName(final String name) {
+        Validate.notNull(name, "name must not be null");
+        try {
+            return entityManager
+                    .createQuery("SELECT c FROM Troop c WHERE name = :name", Troop.class)
+                    .setParameter(":name", name)
+                    .getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
+    }
+    
+    public List<Troop> findAll() {
+        return entityManager.createQuery("SELECT c FROM Troop c", Troop.class).getResultList();
+    }
+
+}

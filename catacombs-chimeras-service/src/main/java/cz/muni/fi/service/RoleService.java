1c936ecf9419c5da0b4cb8bf0ae78e474cf0e2fe
David Osicka <d.osickad@gmail.com>
2016-11-16 11:17:33 +0100

    role

---
+++ catacombs-chimeras-service/src/main/java/cz/muni/fi/service/RoleService.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.service;
+
+import cz.muni.fi.entity.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    /**
+     * find role by id
+     * @param id ID of role
+     * @return role
+     */
+    Role findRoleById(final Long id);
+
+    /**
+     * find role by name
+     * @param name of role
+     * @return role
+     */
+    Role findRoleByName(final String name);
+
+    /**
+     * find all roles
+     * @return list of all roles
+     */
+    List<Role> findAllRoles();
+
+    /**
+     * create new role
+     * @param role to create
+     */
+    void createRole(final Role role);
+
+    /**
+     * update role
+     * @param role to update
+     */
+    void updateRole(final Role role);
+
+    /**
+     * remove role specified by id
+     * @param id of role to delete
+     */
+    void removeRole(final Long id);
+}

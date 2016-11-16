deb67c9c9e7e5c0e9992dae783d81c8a49837175
David Osička <d.osickad@gmail.com>
2016-11-16 18:43:39 +0100

    role (#18)
    * role
    
    * code refactoring
    
    * code refactoring

---
+++ catacombs-chimeras-api/src/main/java/cz/muni/fi/dto/RoleCreateDTO.java
@@ -0,0 +1,49 @@
+/**
+ * @author David Osička
+ */
+package cz.muni.fi.dto;
+
+import static org.apache.commons.lang3.Validate.notNull;
+
+public class RoleCreateDTO {
+    private String name;
+    private String description;
+
+    public RoleCreateDTO(final String name, final String description) {
+        this.name = notNull(name);
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        final RoleCreateDTO that = (RoleCreateDTO) o;
+
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
---
+++ catacombs-chimeras-api/src/main/java/cz/muni/fi/facade/RoleFacade.java
@@ -0,0 +1,50 @@
+/**
+ * @author David Osička
+ */
+package cz.muni.fi.facade;
+
+import cz.muni.fi.dto.RoleCreateDTO;
+import cz.muni.fi.dto.RoleDTO;
+
+import java.util.List;
+
+public interface RoleFacade {
+
+    /**
+     * find role by id
+     * @param id ID of role
+     * @return role
+     */
+    RoleDTO findRoleById(final Long id);
+
+    /**
+     * find role by name
+     * @param name of role
+     * @return role
+     */
+    RoleDTO findRoleByName(final String name);
+
+    /**
+     * find all roles
+     * @return list of all roles
+     */
+    List<RoleDTO> findAllRoles();
+
+    /**
+     * create new role
+     * @param role to create
+     */
+    void createRole(final RoleCreateDTO role);
+
+    /**
+     * update role
+     * @param role to update
+     */
+    void updateRole(final RoleDTO role);
+
+    /**
+     * remove role specified by id
+     * @param id of role to delete
+     */
+    void removeRole(final Long id);
+}
